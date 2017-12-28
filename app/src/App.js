652d703078d3beb0945db015c29243377e5aa9f4
Maria B <mbazhlek@gmail.com>
2017-12-28 14:59:24 -0600

    Add function to get quote

--- app/src/App.js
+++ app/src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      quoteAPI: 'http://localhost:8080/api/quote',
+      quote: {}
+    }
+    this.getQuote();
+  }
+
+  getQuote = async () => {
+    const res = await fetch(this.state.quoteAPI);
+    const quote = await res.json();
+    if (quote) {
+      this.setState({ quote });
+      console.log(this.state.quote[0].quoteText);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my quote generator</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { !Object.keys(this.state.quote).length ? "Press button below to get a quote" : this.state.quote[0].quoteText }
         </p>
+        <p className="App-intro">
+          { !Object.keys(this.state.quote).length ? '' : '- ' + this.state.quote[0].quoteAuthor }
+        </p>
+        <button
+          onClick={this.getQuote}
+        >Get quote</button>
       </div>
     );
   }
